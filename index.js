530006f92c3b24ca821fedd30c9bb78b0e18b4cb
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 21:11:47 -0700

    created recursive fundtion to sum numbers between two numbers given

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  // Sum all the values from fromN
+  // up to toN
+  if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN);  //recursive
+  } else {
+    return fromN;
+  }
+};
 
 module.exports = sum;
